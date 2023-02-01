91b7482daf6c473215ae1b520a2f36de52e4022e
Husayn Esmail <31267171+Husayn-Esmail@users.noreply.github.com>
2023-02-01 01:32:54 -0700

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+echo "Creating the virtual environment"
+python3 -m venv venv
+source venv/bin/activate
+echo "cloning and installing dht library"
 sudo apt-get install git build-essential python-dev -y
 git clone https://github.com/adafruit/Adafruit_Python_DHT.git
 cd Adafruit_Python_DHT
 sudo python3 setup.py install 
+echo "Installing requirements.txt"
+cd ..
+python3 -m pip install -r requirements.txt
+echo "done! please run start.sh in the virtual environment venv"
 
